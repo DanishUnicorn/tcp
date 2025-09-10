748bda44fc6b85c7a345b422eb3c1603cf4ca639
DanishUnicorn <132076490+DanishUnicorn@users.noreply.github.com>
2025-09-10 19:27:45 +0200

    Not getting errors after the loading of the coreect files in the R data. woopwooop

--- tcp_project_files/R/exercises/e01.r
+++ tcp_project_files/R/exercises/e01.r
@@ -1,21 +1,30 @@
-cat("Titel: Practical Exercise 02 - Tropical Crop Production 2025\n")
+cat("\n\nTitel: Practical Exercise 02 - Tropical Crop Production 2025\n")
 cat("Author: Lucas Daniel Paz Zuleta, TZS159\n")
 cat("Date:", format(Sys.Date(), "%d-%m-%Y"), "\n\n")
 
 # --- Paths ---
-data_path <- "R/data/PE2_germination_data_exercise.csv" # insert the necesary path here
-fig_dir   <- "R/figures" # leave this be, unless you want to change the figure directory
+data_path <- file.path("/Users/bruger/Desktop/Folders/KU/Master/year_2/blok_01/TCP/tcp_notes/tcp_project_files/R/data/PE2_germination_data_exercise.csv") # Update the path to the correct file in your data folder
+fig_dir   <- file.path("/Users/bruger/Desktop/Folders/KU/Master/year_2/blok_01/TCP/tcp_notes/tcp_project_files/R/figures") # Leave this as is, unless you want to change the figure directory
 
-if (!dir.exists(fig_dir)) dir.create(fig_dir)
+# Check if the figures directory exists, create it if not
+if (!dir.exists(fig_dir)) dir.create(fig_dir, recursive = TRUE)
+
+# Check if the data file exists
+if (!file.exists(data_path)) {
+    stop("The data file does not exist at the specified path: ", data_path)
+}
 
 # This code is used for the lecture and the exercise.
 
-# For the exercise you might need to change the number of parameters and the best model to use for plotting. Look for the "CLUES"
-# Remember to format the exercise data to fit the code (column names, format etc.)
+# For the exercise you might need to change the number of parameters and 
+#the best model to use for plotting. Look for the "CLUES"
+# Remember to format the exercise data to fit the code 
+# (column names, format etc.)
 
 
 # --- Package & Git Repo installation ---
-# If the following has not been installed yet, please ensure to install it before running the code. Otherwise, you can skip this part.
+# If the following has not been installed yet, please ensure to install it 
+# before running the code. Otherwise, you can skip this part.
 
 # install.packages("dplyr")
 # install.packages("ggplot2")
@@ -35,10 +44,9 @@ library(ggplot2)
 
 # Reading the dataset from R/data/
 
-m <- read.csv("R/data/PE2_germination_data_exercise.csv")   # European CSVs → use read.csv2()
+m <- read.csv(data_path)   # European CSVs → use read.csv2()
 
 head(m)
 tail(m)
-
 
 
